c02b55307503df340c50693f48c7faaa17f33577
Zhifu Ge <zhifu@me.com>
2018-10-01 17:00:40 -0400

    Add the project

---
+++ CCSIntegerPickerViewController/CCSIntegerPickerViewController.h
@@ -0,0 +1,19 @@
+//
+//  CCSIntegerPickerViewController.h
+//  CCSIntegerPickerViewController
+//
+//  Created by Zhifu Ge on 2018-10-01.
+//  Copyright © 2018 Cocoa Swiftly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCSIntegerPickerViewController.
+FOUNDATION_EXPORT double CCSIntegerPickerViewControllerVersionNumber;
+
+//! Project version string for CCSIntegerPickerViewController.
+FOUNDATION_EXPORT const unsigned char CCSIntegerPickerViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCSIntegerPickerViewController/PublicHeader.h>
+
+
